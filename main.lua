97d1622276240f569a17a9ed67132513ae6d0311
Seppi <josefnpat@hotmail.com>
2012-11-20 02:04:05 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,36 @@
+require("lovesplash/lovesplash")
+
+state = "lovesplash"
+
+function love.draw()
+  if state == "lovesplash" then
+    lovesplash.draw()
+  end
+end
+
+function love.update(dt)
+  if state == "lovesplash" then
+    lovesplash.update(dt)
+    if lovesplash.done() then
+      state = "notlovesplash"
+    end
+  end
+end
+
+function love.keypressed()
+  if state == "lovesplash" then
+    lovesplash.stop()
+    state = "notlovesplash"
+  else
+    love.event.quit()
+  end
+end
+
+function love.mousepressed()
+  if state == "lovesplash" then
+    lovesplash.stop()
+    state = "notlovesplash"
+  else
+    love.event.quit()
+  end
+end
---
+++ readme.md
@@ -0,0 +1,4 @@
+Thank you to Daniel Bienvenu for allowing usage of his cover of "What is Love".
+http://ccjvq.com/newcoleco/
+
+I have contacted Daniel Bienvenu, and he has graciously allowed usage of his song, "cv - what is love" for the love community's games under the condition that Daniel Bienvenu is credited.
